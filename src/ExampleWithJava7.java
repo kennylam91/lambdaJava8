b1555db2ebde0ab8dd577a379175bba89680a731
kennylam91 <phamvanlam1991@gmail.com>
2020-05-30 08:44:56 +0700

    first init

---
+++ src/ExampleWithJava7.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class ExampleWithJava7 {
+
+	public static void main(String[] args) {
+		
+		List<Person> people = Arrays.asList(
+				new Person("PHAM VAN", "LAM", 30L),
+				new Person("HOANG TRUNG", "VAN", 20L),
+				new Person("DO VAN", "THANG", 18L),
+				new Person("NGUYEN VAN", "HOANG", 25L),
+				new Person("Le VAN", "TOAN", 40L)
+				);
+		
+		// Step 1: Sort this list by first name
+		
+		// Step 2: print all people
+		
+		// Step 3: Print all people with lastname start with T
+		
+	}
+}
---
+++ src/Person.java
@@ -0,0 +1,42 @@
+
+public class Person {
+	private String firstName;
+	
+	private String lastName;
+	
+	private Long age;
+	
+
+	public Person(String firstName, String lastName, Long age) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.age = age;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public Long getAge() {
+		return age;
+	}
+
+	public void setAge(Long age) {
+		this.age = age;
+	}
+	
+	
+}
